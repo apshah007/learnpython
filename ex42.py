95969ea32735188e4d1f74745fb1e5f4a1fb9028
Amy Shah <amyshah@Amys-MacBook-Pro-2.local>
2018-04-09 21:45:05 -0400

    exercise 42

--- ex42.py
+++ ex42.py
@@ -2,70 +2,70 @@
 class Animal(object):
     pass
 
-## ??
+## is a
 class Dog(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## has a
         self.name = name
 
-## ??
+## is a
 class Cat(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## has a
         self.name = name
 
-## ??
+## has a
 class Person(object):
 
     def __init__(self, name):
-        ## ??
+        ## has a
         self.name = name
         ## Person has a pet of some kind
         self.pet = None
 
-##??
+## is a
 class Employee(Person):
     def __init__(self, name, salary):
         ##?? hmm what is this strange magic?
         super(Employee, self).__init__(name)
-        ##??
+        ## has a
         self.salary = salary
 
-##??
+## has a
 class Fish(object):
     pass
-
+## is a
 class Salmon(Fish):
     pass
-
+## is a
 class Halibut(Fish):
     pass
 
 ## rover is-a Dog
 rover = Dog("Rover")
 
-## ??
+## is a
 satan = Cat("Satan")
 
-##??
+## is a
 mary = Person("Mary")
 
-## ??
+## has a
 mary.pet = satan
 
-## ??
+## is a
 frank = Employee("Frank", 120000)
 
-##??
+## has a
 frank.pet = rover
 
-##??
+## is a
 flipper = Fish()
 
-##??
+## has a
 crouse = Salmon()
 
-##??
+##?? is a 
 harry = Halibut()
